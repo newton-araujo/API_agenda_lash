2e18f5fe991f34829d8fe8de63d705b1cbc15021
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2026-09-05 22:13:48 -0300

    update_app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from src.routes.users import users
 from src.routes.login import login_auth
 from src.routes.clientes import client
+from src.routes.procedures import procedure
 
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(users)
 app.register_blueprint(login_auth)
 app.register_blueprint(client)
+app.register_blueprint(procedure)
 
 
 if __name__ == "__main__":
